121a6bd8f42edfa7d6701895fa47831ce5c8408c
Ikbol <muxtorovikboljon1@gmail.com>
2024-03-01 15:19:14 +0500

    bot_telegram_py faylini qushdim

---
+++ main.py
@@ -0,0 +1,148 @@
+
+'''
+1 chi ish
+'''
+
+
+# named tuples
+
+# class User:
+#     def __init__(self, id, name, last_name, age, email):
+#         self.id = id
+#         self.name = name
+#         self.last_name = last_name
+#         self.age = age
+#         self.email = email
+#
+#
+#
+# user = (1, 'Toxir', 'Toxirov', 28, 'toxir@gmail.com')
+
+
+#=============================================================================================
+
+
+# from collections import namedtuple
+#
+# User = namedtuple('User', 'id, name, last_name, age, email')
+#
+#
+# users = [
+#     (1, 'Toxir', 'Toxirov', 28, 'toxir@gmail.com'),
+#     (2, 'Sobir', 'Toxirov', 29, 'sobir@gmail.com'),
+#     (3, 'Toxir', 'Bakirov', 40, 'bakir@gmail.com')
+# ]
+#
+# for user in users:
+#     us = User(*user)
+#     print(*us)
+
+
+
+#=============================================================================================
+
+# from collections import namedtuple
+#
+# User = namedtuple('User', 'name color probek price joy roles')
+#
+# car = ('Malibu', 'Rad', '300 km/h', 20000, 4, 'Avtomat')
+#
+# employee = User(*car)
+# print(employee)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
